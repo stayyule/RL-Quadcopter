be6dde67b6f612c1770fdbd4073c4e4060553bca
Eric YU <leyu@ixiacom.com>
2019-06-10 22:00:48 +0800

    task update

--- quad_controller_rl/src/quad_controller_rl/tasks/hover.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/hover.py
@@ -80,7 +80,7 @@ class Hover(BaseTask):
                           + abs(linear_acceleration.y) * self.lin_y_alpha
                        ) * self.alpha
         else:
-            reward = (pose.position.z -
+            reward = (pose.position.z * self.pos_z_alpha -
                         (abs(self.target_x - pose.position.x) * self.pos_x_alpha
                           + abs(self.target_y - pose.position.y) * self.pos_y_alpha)
                         ) * self.alpha
