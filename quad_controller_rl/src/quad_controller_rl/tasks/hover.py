7f4dced317819bc9b18a944cf04f3f98ac3d1468
Eric YU <leyu@ixiacom.com>
2019-06-13 22:37:23 +0800

    task update

--- quad_controller_rl/src/quad_controller_rl/tasks/hover.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/hover.py
@@ -85,10 +85,10 @@ class Hover(BaseTask):
         done = False
         
         reward_alpha = 0.8
-        reward_beta = 0.1
+        reward_beta = 0.01
 
         distance_reward = (10.0 - abs(del_z)) * reward_alpha
-        accelerate_reward = abs(linear_acceleration.z) * reward_beta
+        accelerate_reward = abs(linear_vel) * reward_beta
         if pose.position.z < 5:
             reward = distance_reward
         else:
