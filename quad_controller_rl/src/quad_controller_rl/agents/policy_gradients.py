c3e79fd07c5e83c3caa9da1463a921e8bbb6076f
Eric YU <leyu@ixiacom.com>
2019-06-15 14:45:27 +0800

    task update

--- quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
+++ quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
@@ -47,7 +47,7 @@ class DDPG(BaseAgent):
 
         # Replay memory
         self.buffer_size = 100000
-        self.batch_size = 256
+        self.batch_size = 1024
         self.memory = ReplayBuffer(self.buffer_size)
 
         # Algorithm parameters
