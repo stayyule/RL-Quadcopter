adf981b96be752c6bf926b4cbc5b5cf42a22c52c
Eric YU <leyu@ixiacom.com>
2019-06-17 23:48:17 +0800

    task update

--- quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
+++ quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
@@ -319,7 +319,7 @@ class ReplayBuffer:
             self.idx = (self.idx + 1) % self.size
     
     def sample(self, batch_size=64):
-        sorted(self.memory, key=self.get_reward, reserve=True)
+        sorted(self.memory, key=self.get_reward, reverse=True)
         return self.memory[:batch_size]
         #"""Randomly sample a batch of experiences from memory."""
         #return random.sample(self.memory, k=batch_size)
