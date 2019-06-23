e33c39b4ebbea24719c53bbe80fb9ac21e612765
Eric YU <leyu@ixiacom.com>
2019-06-23 22:05:31 +0800

    agent update

--- quad_controller_rl/src/quad_controller_rl/tasks/hover.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/hover.py
@@ -80,11 +80,11 @@ class Hover(BaseTask):
         # Compute reward / penalty and check if this episode is complete
         done = False
         
-        reward_alpha = 1
-        reward_beta = 0.03
+        reward_alpha = 0.7
+        reward_beta = 0.08
         distance = np.power(np.power(del_x, 2) + np.power(del_y, 2) + np.power(del_z, 2), 0.5)
 
-        distance_reward = (10.0 - distance) * reward_alpha
+        distance_reward = (5.0 - distance) * reward_alpha
 
 #        if pose.position.z < 1:
 #            accelerate_reward = 0
@@ -97,11 +97,13 @@ class Hover(BaseTask):
 
         reward = distance_reward - accelerate_reward
         
-        print('height:', pose.position.z)
-        #print('state:', state)
-        print('reward:', reward)
-        print('distance:', distance_reward)
-        print('accelerate:', accelerate_reward)
+        if pose.position.z > 8:
+            print('==========')
+            print('height:', pose.position.z)
+            #print('state:', state)
+            print('reward:', reward)
+            print('distance:', distance_reward)
+            print('accelerate:', accelerate_reward)
 
         if timestamp > self.max_duration:  # agent has run out of time
             #reward -= 10.0  # extra penalty
