6f5f482fde6b89281e5e2e00627f998570d82297
Eric YU <leyu@ixiacom.com>
2019-06-26 17:54:55 +0800

    agent update

--- quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
+++ quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
@@ -229,7 +229,7 @@ class Actor:
         
         #loss = K.mean(-action_gradients * actions)
 
-        loss = K.mean(action_gradients * actions)
+        loss = -1 * K.mean(action_gradients * actions)
 
         # Incorporate any additional losses here (e.g. from regularizers)
 
