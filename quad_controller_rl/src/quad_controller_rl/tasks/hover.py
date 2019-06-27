fd3e27d590a8a13c0b8c074b118923eae3a85414
Eric YU <leyu@ixiacom.com>
2019-06-27 12:01:55 +0800

    agent update

--- quad_controller_rl/src/quad_controller_rl/tasks/hover.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/hover.py
@@ -88,7 +88,7 @@ class Hover(BaseTask):
         done = False
         
         reward_alpha = 1
-        reward_beta = 0.05
+        reward_beta = 0.1
         distance = np.power(np.power(del_x, 2) + np.power(del_y, 2) + np.power(del_z, 2), 0.5)
 
         distance_reward = (5.0 - distance) * reward_alpha
