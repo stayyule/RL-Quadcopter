bac5b1d07b86e96919ca852a9385e949e282d602
Eric YU <leyu@ixiacom.com>
2019-06-29 22:08:58 +0800

    task update

--- quad_controller_rl/src/quad_controller_rl/tasks/hover.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/hover.py
@@ -118,7 +118,7 @@ class Hover(BaseTask):
         print('target:', self.target_z)
 
         if self.target_z == 10.0:
-            print('extra reward:', del_z < 0.1)
+            print('extra reward:', del_z < 0.1, del_z)
             if del_z < 0.1:
                 reward += 1.0
             else:
