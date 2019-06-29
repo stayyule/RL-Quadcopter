534c321d3639db8863ea451ecc17c9da81ceda1e
Eric YU <leyu@ixiacom.com>
2019-06-29 22:31:59 +0800

    task update

--- quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
+++ quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
@@ -130,9 +130,9 @@ class DDPG(BaseAgent):
         #else:
         #    return np.around(noise_epsilon * noise_val, decimals=2)
         #tanh
-        final_action = np.clip(np.around(actions + noise_val * noise_epsilon, decimals=2), -1, 1)
+        #final_action = np.clip(np.around(actions + noise_val * noise_epsilon, decimals=2), -1, 1)
         #sigmoid
-        #final_action = np.clip(np.around(actions + noise_val * noise_epsilon, decimals=2), 0, 1)
+        final_action = np.clip(np.around(actions + noise_val * noise_epsilon, decimals=2), 0, 1)
         print("predict:", np.around(actions, decimals=2), "noise:", np.around(noise_val * noise_epsilon, decimals=2), "action:", np.around(final_action,decimals=2))
 
         return final_action
