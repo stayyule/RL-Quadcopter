5be0835ffc5243314a533f7268c7acdc49acdfae
Eric YU <leyu@ixiacom.com>
2019-06-29 14:09:31 +0800

    task update

--- quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
+++ quad_controller_rl/src/quad_controller_rl/agents/policy_gradients.py
@@ -219,7 +219,7 @@ class Actor:
 
         # Add final output layer with sigmoid activation
         # -----kernel
-        actions = layers.Dense(units=self.action_size, activation='sigmoid',
+        actions = layers.Dense(units=self.action_size, activation='tanh',
         name='raw_actions',
         kernel_initializer=layers.initializers.RandomUniform(minval=-3e-3,maxval=3e-3))(net)
 
