19a16265bd0a09618cdeb918ee98006af5116b33
Eric YU <leyu@ixiacom.com>
2019-06-29 16:43:52 +0800

    task update

--- quad_controller_rl/src/quad_controller_rl/tasks/combined.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/combined.py
@@ -101,7 +101,7 @@ class Combined(BaseTask):
         reward_beta = 0.05
         distance = np.power(np.power(del_x, 2) + np.power(del_y, 2) + np.power(del_z, 2), 0.5)
 
-        distance_reward = (5.0 - distance) * reward_alpha
+        distance_reward =  - distance * reward_alpha
 
 #        if pose.position.z < 1:
 #            accelerate_reward = 0
