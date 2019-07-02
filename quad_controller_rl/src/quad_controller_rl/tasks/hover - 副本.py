003716a3dc0704f3502b271756239e2dbaf8ee32
Eric YU <leyu@ixiacom.com>
2019-07-02 13:13:43 +0800

    agent and task update

--- quad_controller_rl/src/quad_controller_rl/tasks/hover - 副本.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/hover - 副本.py
@@ -15,6 +15,7 @@ class Hover(BaseTask):
             np.array([  cube_size / 2,   cube_size / 2, cube_size,  1.0,  1.0,  1.0,  1.0]))        
             
         #print("Takeoff(): observation_space = {}".format(self.observation_space))  # [debug]
+        self.observation_space_range = self.observation_space.high - self.observation_space.low
 
         # Action space: <force_x, .._y, .._z, torque_x, .._y, .._z>
         max_force = 25.0
@@ -23,137 +24,87 @@ class Hover(BaseTask):
             np.array([-max_force, -max_force, -max_force, -max_torque, -max_torque, -max_torque]),
             np.array([ max_force,  max_force,  max_force,  max_torque,  max_torque,  max_torque]))
         #print("Takeoff(): action_space = {}".format(self.action_space))  # [debug]
+        self.action_space_range = self.action_space.high - self.action_space.low
 
         # Task-specific parameters
-        self.max_duration = 8.0  # secs
-        self.takeoff_duration = 5.0
+        self.max_duration = 10.0  # secs
 
-        self.target_x = 0.0
-        self.target_y = 0.0
-        self.target_z = 10.0  # target height (z position) to reach for successful takeoff
-
-        self.last_x = 0.0
-        self.last_y = 0.0
-        self.last_z = 0.0
-
-
-        self.scale = 15.0
-
-        self.final_target = 10.0
+        self.target = np.array([0.0,0.0,10.0])
+        self.last_pos = np.array([0.0,0.0,0.0])
+        self.start = np.array([0.0,0.0,10.0])
         self.last_time = 0.0
-        self.last_action = None
+        self.count = 0
 
     def reset(self):
-        self.last_x = 0.0
-        self.last_y = 0.0
-        self.last_z = 0.0
-
+        self.last_pos = self.start
         self.action = None
-        self.last_action = None
         self.last_time = 0.0
+        self.count = 0
         # Nothing to reset; just return initial condition
         return Pose(
-                position=Point(0.0, 0.0, np.random.normal(0.5, 0.1)),  # drop off from a slight random height
+                position=Point(0.0, 0.0, self.start[2]),  # drop off from a slight random height
                 orientation=Quaternion(0.0, 0.0, 0.0, 0.0),
             ), Twist(
                 linear=Vector3(0.0, 0.0, 0.0),
                 angular=Vector3(0.0, 0.0, 0.0)
             )
 
+    def pos_rescale(self,state):
+        #rescaling to (-5.,5.)
+        mid = (self.observation_space.high + self.observation_space.low)*0.5
+        rescaled = 5.0 * ((state - mid[:3])/(self.observation_space_range[:3]*0.5))
+        return rescaled
+
     def update(self, timestamp, pose, angular_velocity, linear_acceleration):
-        # Prepare state vector (pose only; ignore angular_velocity, linear_acceleration)
-
-        scaled_x = pose.position.x / self.scale
-        scaled_y = pose.position.y / self.scale
-        scaled_z = pose.position.z / self.scale
-
-        # vel_x = pose.position.x - self.last_x
-        # vel_y = pose.position.y - self.last_y
-        # vel_z = pose.position.z - self.last_z
-        print('timestamp:', timestamp, 'last time:', self.last_time)
-        if timestamp != self.last_time:
-            vel_x = (pose.position.x - self.last_x) / (timestamp - self.last_time) / self.scale
-            vel_y = (pose.position.y - self.last_y) / (timestamp - self.last_time) / self.scale
-            vel_z = (pose.position.z - self.last_z) / (timestamp - self.last_time) / self.scale
-        else:
-            vel_x = vel_y = vel_z = 0.0
         
-        del_x = (self.target_x - pose.position.x) / self.scale
-        del_y = (self.target_y - pose.position.y) / self.scale
-        del_z = (self.target_z - pose.position.z) / self.scale
+        #position before scaling
+        position = np.array([pose.position.x, pose.position.y, pose.position.z])
 
-        # del_x = self.target_x - pose.position.x
-        # del_y = self.target_y - pose.position.y
-        # del_z = self.target_z - pose.position.z
+        scaled_pos = self.pos_rescale(position)
+        scaled_tar = self.pos_rescale(self.target)
+        # distance vector
+        distance_vec = scaled_tar - scaled_pos
+        # velocity
+        if timestamp != self.last_time:
+            velocity = (position - self.last_pos) / (timestamp - self.last_time)
+        else:
+            velocity = np.array([0.0,0.0,0.0])
 
-        state = np.around(np.array([
-                scaled_x, scaled_y, scaled_z,
-                vel_z,
-                del_z,
-                linear_acceleration.z,
-                timestamp / 10.0 ]), decimals=2)
-        # state = np.around(np.array([
-        #         scaled_x, scaled_y, scaled_z,
-        #         vel_x * 10.0, vel_y * 10.0, vel_z * 10.0,
-        #         del_z ]), decimals=2)
-        # state = np.around(np.array([
-        #         pose.position.x, pose.position.y, pose.position.z,
-        #         linear_acceleration.x, linear_acceleration.y, linear_acceleration.z,
-        #         del_z ]), decimals=2)
+        state = np.concatenate((scaled_pos, velocity, distance_vec * 5.0),axis=-1)
 
-        self.last_x = pose.position.x
-        self.last_y = pose.position.y
-        self.last_z = pose.position.z
+        self.last_pos = position
         self.last_time = timestamp
 
         # Compute reward / penalty and check if this episode is complete
         done = False
         
-        reward_alpha = 0.5
-        reward_beta = 0.5
+        reward_alpha = 0.3
+        reward_beta = 0.05
 
-        #distance = np.linalg.norm([del_x, del_y, del_z])
-        distance = min(abs(self.target_z - pose.position.z), 10.0)
+        # distance value
+        distance = np.linalg.norm(self.target - position)
+        accelerate = np.linalg.norm(linear_acceleration)
 
-        distance_reward = (10 - distance) * reward_alpha
-        # accelerate_reward = abs(vel_z) * reward_beta
-        if self.last_action is not None:
-            accelerate_reward = np.linalg.norm(self.last_action) * reward_beta
-        else:
-            accelerate_reward = 0
-
-        #reward = distance_reward - accelerate_reward
-        if pose.position.z <= 5.0:
-            reward = distance_reward
-        else:
-            reward = distance_reward - accelerate_reward
-
-        print('==========')
-        print('height:', pose.position.z)
-        print('reward:', reward)
-        print('accelerate:', accelerate_reward)
-        
-        # target_val = max(self.final_target / self.takeoff_duration * timestamp, 5)
-
-        # self.target_z = min (np.around(target_val), 10.0)
-        # print('target:', self.target_z)
-
+        distance_reward = (5 - distance) * reward_alpha
+        accelerate_reward = accelerate * reward_beta
+        reward = distance_reward - accelerate_reward
+        print('reward={:.3} distance={:.3} target={:.3}'.format(reward ,distance_vec[2],self.target[2]),end='\r')
+            
         if timestamp > self.max_duration:  # agent has run out of time
             #reward -= 10.0  # extra penalty
-            done = True
-
-        if pose.position.z > 20.0:
             done = True
 
         # Take one RL step, passing in current state and reward, and obtain action
         # Note: The reward passed in here is the result of past action(s)
         action = self.agent.step(state, reward, done)  # note: action = <force; torque> vector
-        self.last_action = action / 25.0
 
-        self.agent.write_sa([pose.position.x, pose.position.y, pose.position.z,
-                        vel_z, self.target_z - pose.position.z, linear_acceleration.z,
-                        action[2], reward])
+        # self.agent.write_sa([pose.position.x, pose.position.y, pose.position.z,
+        #                 vel_z, self.target_z - pose.position.z, linear_acceleration.z,
+        #                 action[2], reward])
 
+        self.agent.write_sa([scaled_pos[0], scaled_pos[1], scaled_pos[2],
+                        velocity[2], distance_vec[2], linear_acceleration.z,
+                        action[2]/ 25.0,  reward])       
 
         # Convert to proper force command (a Wrench object) and return it
         if action is not None:
--- quad_controller_rl/src/quad_controller_rl/tasks/hover.py
+++ quad_controller_rl/src/quad_controller_rl/tasks/hover.py
@@ -1,5 +1,4 @@
-"""Hover  task."""
-from collections import deque
+"""Takeoff task."""
 
 import numpy as np
 from gym import spaces
@@ -10,11 +9,11 @@ class Hover(BaseTask):
     """Simple task where the goal is to lift off the ground and reach a target height."""
 
     def __init__(self):
-        # State space: <position_x, .._y, .._z, orientation_x, .._y, .._z, .._w>
         cube_size = 300.0  # env is cube_size x cube_size x cube_size
         self.observation_space = spaces.Box(
             np.array([- cube_size / 2, - cube_size / 2,       0.0, -1.0, -1.0, -1.0, -1.0]),
-            np.array([  cube_size / 2,   cube_size / 2, cube_size,  1.0,  1.0,  1.0,  1.0]))
+            np.array([  cube_size / 2,   cube_size / 2, cube_size,  1.0,  1.0,  1.0,  1.0]))        
+            
         #print("Takeoff(): observation_space = {}".format(self.observation_space))  # [debug]
         self.observation_space_range = self.observation_space.high - self.observation_space.low
 
@@ -29,58 +28,85 @@ class Hover(BaseTask):
 
         # Task-specific parameters
         self.max_duration = 10.0  # secs
-        self.count = 0
-        self.action = None
-        self.last_time = 0.0
-        self.start = np.array([0.0,0.0,10.0])
+
         self.target = np.array([0.0,0.0,10.0])
         self.last_pos = np.array([0.0,0.0,0.0])
+        self.start = np.array([0.0,0.0,10.0])
+        self.last_time = 0.0
+        self.count = 0
 
     def reset(self):
-        self.count = 0
+        self.last_pos = self.start
         self.action = None
         self.last_time = 0.0
-        rand_z = np.random.normal(0.5, 0.1)
-        self.start[2] = rand_z
-        self.last_pos = self.start
+        self.count = 0
+        # Nothing to reset; just return initial condition
         return Pose(
-                position=Point(0.0, 0.0,self.start[2]),  # drop off from a slight random height
+                position=Point(0.0, 0.0, self.start[2]),  # drop off from a slight random height
                 orientation=Quaternion(0.0, 0.0, 0.0, 0.0),
             ), Twist(
                 linear=Vector3(0.0, 0.0, 0.0),
                 angular=Vector3(0.0, 0.0, 0.0)
             )
-    def state_rescale(self,state):
-        """rescaling to (-5.,5.)"""
+
+    def pos_rescale(self,state):
+        #rescaling to (-5.,5.)
         mid = (self.observation_space.high + self.observation_space.low)*0.5
-        rescaled = 5. * ((state - mid[:3])/(self.observation_space_range[:3]*0.5))
+        rescaled = 5.0 * ((state - mid[:3])/(self.observation_space_range[:3]*0.5))
         return rescaled
 
     def update(self, timestamp, pose, angular_velocity, linear_acceleration):
-        self.count += 1
-        pos = np.array([pose.position.x, pose.position.y, pose.position.z])
-        distance = np.linalg.norm(self.target-pos)
-        rescaled_pos = self.state_rescale(pos)
-        rescaled_tar = self.state_rescale(self.target)
-        pos_target_vec = rescaled_tar - rescaled_pos
-        delta_pos = (pos-self.last_pos)
-        self.last_pos = pos
-        state = np.concatenate((rescaled_pos,delta_pos*10.,pos_target_vec*5.0),axis=-1)
-        #state = state[None,:]
-        accel = np.array([linear_acceleration.x, linear_acceleration.y, linear_acceleration.z])
-        sum_accel = np.linalg.norm(accel)
-        done = False
-        reward = (5.-distance)*0.3 - sum_accel*0.05
-        print('re={:.3} pt={:.3} tar={:.3}'.format(reward ,pos_target_vec[2],self.target[2]),end='\r')
+        
+        #position before scaling
+        position = np.array([pose.position.x, pose.position.y, pose.position.z])
 
+        scaled_pos = self.pos_rescale(position)
+        scaled_tar = self.pos_rescale(self.target)
+        # distance vector
+        distance_vec = scaled_tar - scaled_pos
+        # velocity
+        if timestamp != self.last_time:
+            velocity = (position - self.last_pos) / (timestamp - self.last_time)
+        else:
+            velocity = np.array([0.0,0.0,0.0])
+
+        state = np.concatenate((scaled_pos, velocity, distance_vec * 5.0),axis=-1)
+
+        self.last_pos = position
+        self.last_time = timestamp
+
+        # Compute reward / penalty and check if this episode is complete
+        done = False
+        
+        reward_alpha = 0.3
+        reward_beta = 0.05
+
+        # distance value
+        distance = np.linalg.norm(self.target - position)
+        accelerate = np.linalg.norm(linear_acceleration)
+
+        distance_reward = (5 - distance) * reward_alpha
+        accelerate_reward = accelerate * reward_beta
+        reward = distance_reward - accelerate_reward
+        print('reward={:.3} distance={:.3} target={:.3}'.format(reward ,distance_vec[2],self.target[2]),end='\r')
+            
         if timestamp > self.max_duration:  # agent has run out of time
+            #reward -= 10.0  # extra penalty
             done = True
 
-        action = self.agent.step(state, reward, done)
+        # Take one RL step, passing in current state and reward, and obtain action
+        # Note: The reward passed in here is the result of past action(s)
+        action = self.agent.step(state, reward, done)  # note: action = <force; torque> vector
+
+        # self.agent.write_sa([pose.position.x, pose.position.y, pose.position.z,
+        #                 vel_z, self.target_z - pose.position.z, linear_acceleration.z,
+        #                 action[2], reward])
+
+        self.agent.write_sa([scaled_pos[0], scaled_pos[1], scaled_pos[2],
+                        velocity[2], distance_vec[2], linear_acceleration.z,
+                        action[2]/ 25.0,  reward])       
 
         # Convert to proper force command (a Wrench object) and return it
-        #action = self.action * self.action_space_range[2] * 0.5#only z force
-        
         if action is not None:
             action = np.clip(action.flatten(), self.action_space.low, self.action_space.high)  # flatten, clamp to action space limits
             return Wrench(
@@ -88,4 +114,5 @@ class Hover(BaseTask):
                     torque=Vector3(action[3], action[4], action[5])
                 ), done
         else:
+
             return Wrench(), done
